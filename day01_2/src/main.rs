d8a08d090a25b1550c5d37953ffb37a1bf0f85c1
panicbit <panicbit.dev@gmail.com>
2019-12-02 20:52:15 +0100

    day01_2

--- day01_2/src/main.rs
+++ day01_2/src/main.rs
@@ -1,6 +1,18 @@
 #[macro_use] extern crate aoc;
 
+use std::iter::successors;
+
 #[aoc(2019, 01, 2)]
-fn main(input: &str) -> i32 {
-    unimplemented!()
+fn main(input: &str) -> f32 {
+    input
+    .lines()
+    .map(|s| {
+        let mass = s.parse::<f32>().unwrap();
+        let fuel = successors(Some(mass), |mass| Some((mass / 3.).floor() - 2.))
+            .skip(1)
+            .take_while(|&fuel| fuel > 0.)
+            .sum::<f32>();
+        fuel
+    })
+    .sum()
 }
