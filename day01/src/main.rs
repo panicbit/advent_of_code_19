fedd28da9dde06500cc63dc16699d2ce96478fc1
panicbit <panicbit.dev@gmail.com>
2019-12-02 20:04:31 +0100

    day01

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,6 +1,13 @@
 #[macro_use] extern crate aoc;
 
 #[aoc(2019, 01, 1)]
-fn main(input: &str) -> i32 {
-    unimplemented!()
+fn main(input: &str) -> f32 {
+    input
+    .lines()
+    .map(|s| {
+        let mass = s.parse::<f32>().unwrap();
+        let fuel = (mass / 3.).floor() - 2.;
+        fuel
+    })
+    .sum()
 }
