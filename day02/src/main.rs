a245a52a543a90847202932c38f5a2910359b9a3
panicbit <panicbit.dev@gmail.com>
2019-12-02 21:46:26 +0100

    day02

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,6 +1,34 @@
 #[macro_use] extern crate aoc;
 
 #[aoc(2019, 02, 1)]
-fn main(input: &str) -> i32 {
-    unimplemented!()
+fn main(input: &str) -> usize {
+    let mut mem = input
+        .trim()
+        .split(',')
+        .map(|cell| cell.parse::<usize>().unwrap())
+        .collect::<Vec<_>>();
+
+    // "Restore gravity assist program"
+    mem[1] = 12;
+    mem[2] = 2;
+
+    let mut ip = 0;
+
+    loop {
+        let opcode = mem[ip];
+        let a = mem[ip + 1];
+        let b = mem[ip + 2];
+        let target = mem[ip + 3];
+
+        match opcode {
+            1 => mem[target] = mem[a] + mem[b],
+            2 => mem[target] = mem[a] * mem[b],
+            99 => break,
+            code => unreachable!("code: {}", code),
+        }
+
+        ip += 4;
+    }
+
+    mem[0]
 }
