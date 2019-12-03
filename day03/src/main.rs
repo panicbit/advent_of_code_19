4d6510fbe38ca4a7473feed2b6bfa7404b2a0749
panicbit <panicbit.dev@gmail.com>
2019-12-03 09:00:25 +0100

    day03

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,6 +1,60 @@
 #[macro_use] extern crate aoc;
 
+use std::collections::{HashMap, HashSet};
+
 #[aoc(2019, 03, 1)]
 fn main(input: &str) -> i32 {
-    unimplemented!()
+    let wires = input
+        .lines()
+        .enumerate()
+        .map(|(id, wire)| {
+            wire
+            .split(",")
+            .map(|part| {
+                let dir = &part[0..1];
+                let len = part[1..].parse::<i32>().unwrap();
+                Part { id, dir, len }
+            })
+            .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+
+    let mut trace = HashMap::<(i32, i32), HashSet<usize>>::new();
+
+    for wire in wires {
+        let mut x = 0;
+        let mut y = 0;
+
+        for part in wire {
+            for _ in 0..part.len {
+                match part.dir {
+                    "R" => x += 1,
+                    "L" => x -= 1,
+                    "U" => y += 1,
+                    "D" => y -= 1,
+                    _ => unreachable!(),
+                }
+
+                trace
+                    .entry((x, y))
+                    .or_default()
+                    .insert(part.id);
+            }
+        }
+    }
+
+    let min_crossing_distance = trace
+        .into_iter()
+        .filter(|(_, crossings)| crossings.len() > 1)
+        .map(|((x, y), _)| x.abs() + y.abs())
+        .min()
+        .unwrap_or(0);
+
+    min_crossing_distance
+}
+
+struct Part<'a> {
+    id: usize,
+    len: i32,
+    dir: &'a str,
 }
