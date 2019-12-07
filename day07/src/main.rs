2946d676f8a06bdca42ca99ed27f48f35b9b16e4
panicbit <panicbit.dev@gmail.com>
2019-12-07 15:13:36 +0100

    day07

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -1,6 +1,32 @@
 #[macro_use] extern crate aoc;
 
+use itertools::Itertools;
+
 #[aoc(2019, 07, 1)]
-fn main(input: &str) -> i32 {
-    unimplemented!()
+fn main(input: &str) -> isize {
+    let mem = intcode::parse(input);
+
+    (0..=4)
+    .permutations(5)
+    .map(|phases| run_series(&mem, phases))
+    .max()
+    .unwrap()
+}
+
+fn run_series(mem: &[isize], phases: Vec<isize>) -> isize {
+    let mut signal = 0;
+
+    for phase in phases {
+        signal = run_stage(mem.clone(), phase, signal);
+    }
+
+    signal
+}
+
+fn run_stage(mem: &[isize], phase: isize, signal: isize) -> isize {
+    let mut vm = intcode::VM::new(mem);
+    vm.add_input(phase);
+    vm.add_input(signal);
+    vm.run();
+    vm.outputs()[0]
 }
