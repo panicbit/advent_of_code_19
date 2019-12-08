2f4be6abb780269f591aaeb2dcf8a34aa10a4260
panicbit <panicbit.dev@gmail.com>
2019-12-08 13:25:28 +0100

    day08 & day08_2 => single expression

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -4,60 +4,31 @@ use itertools::Itertools;
 
 #[aoc(2019, 08, 1)]
 fn main(input: &str) -> usize {
-    let width = 25;
-    let height = 6;
-    let stride = width * height;
-    let layers: Vec<Layer> =
-        input
-        .chars()
-        .map(|c| c.to_digit(10))
-        .flatten()
-        .chunks(stride)
-        .into_iter()
-        .map(|layer| Layer::parse(layer))
-        .collect();
-
-    let layer_with_fewest_zeros =
-        layers
+    input
+    .chars()
+    .map(|c| c.to_digit(10))
+    .flatten()
+    .chunks(25 * 6)
+    .into_iter()
+    .map(|layer| layer.collect_vec())
+    .collect_vec()
+    .into_iter()
+    .min_by_key(|layer|
+        layer
         .iter()
-        .min_by_key(|layer|
-            layer.count_pixels(|pixel| pixel == 0)
-        )
-        .unwrap();
-
-    let num_1_digits =
-        layer_with_fewest_zeros
-        .count_pixels(|pixel| pixel == 1);
-
-    let num_2_digits =
-        layer_with_fewest_zeros
-        .count_pixels(|pixel| pixel == 2);
-
-    num_1_digits * num_2_digits
-}
-
-struct Layer {
-    pixels: Vec<u32>,
-}
-
-impl Layer {
-    fn parse(pixels: impl Iterator<Item = u32>) -> Self {
-        Layer {
-            pixels: pixels.collect()
-        }
-    }
-
-    fn pixels(&self) -> impl Iterator<Item = u32> + '_ {
-        self
-        .pixels
-        .iter()
-        .copied()
-    }
-
-    fn count_pixels(&self, mut f: impl FnMut(u32) -> bool) -> usize {
-        self
-        .pixels()
-        .filter(|&pixel| f(pixel))
+        .filter(|&&pixel| pixel == 0)
         .count()
-    }
+    )
+    .map(|layer| {
+        layer
+        .iter()
+        .filter(|&&pixel| pixel == 1)
+        .count()
+        *
+        layer
+        .iter()
+        .filter(|&&pixel| pixel == 2)
+        .count()
+    })
+    .unwrap()
 }
--- day08_2/src/main.rs
+++ day08_2/src/main.rs
@@ -3,74 +3,39 @@
 use itertools::Itertools;
 
 #[aoc(2019, 08, 2)]
-fn main(input: &str) -> usize {
-    let width = 25;
-    let height = 6;
-    let stride = width * height;
-    let layers: Vec<Layer> =
+fn main(input: &str) -> i32 {
+    println!("{}",
         input
         .chars()
         .map(|c| c.to_digit(10))
         .flatten()
-        .chunks(stride)
+        .chunks(25 * 6)
         .into_iter()
-        .map(|layer| Layer::parse(layer))
-        .collect();
-
-    let merged_layers =
-        layers
+        .map(|row| row.collect_vec())
+        .collect_vec()
         .into_iter()
         .rev()
-        .fold1(|a, b| a.apply(&b))
-        .unwrap();
-
-    let output =
-        merged_layers
-        .pixels()
+        .fold1(|a, b| {
+            a
+            .into_iter()
+            .zip(b)
+            .map(|pixels| match pixels {
+                (a, 2) => a,
+                (_, b) => b,
+            })
+            .collect_vec()
+        })
+        .unwrap()
+        .into_iter()
         .map(|pixel| match pixel {
             1 => '█',
             _ => ' ',
         })
-        .chunks(width)
+        .chunks(25)
         .into_iter()
         .map(|mut row| row.join(""))
-        .join("\n");
+        .join("\n")
+    );
 
-    println!("{}", output);
-
-    panic!("HUMAN REQUIRED")
-}
-
-struct Layer {
-    pixels: Vec<u32>,
-}
-
-impl Layer {
-    fn parse(pixels: impl Iterator<Item = u32>) -> Self {
-        Layer {
-            pixels: pixels.collect()
-        }
-    }
-
-    fn pixels(&self) -> impl Iterator<Item = u32> + '_ {
-        self
-        .pixels
-        .iter()
-        .copied()
-    }
-
-    fn apply(&self, layer: &Layer) -> Self {
-        let pixels =
-            self
-            .pixels()
-            .zip(layer.pixels())
-            .map(|pixels| match pixels {
-                (a, 2) => a,
-                (_, b) => b,
-            });
-
-        Self {
-            pixels: pixels.collect(),
-        }
-    }
+    panic!("HUMAN REQUIRED");
 }
