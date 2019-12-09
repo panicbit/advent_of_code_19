4a0ea6d359da4dcf58bdd6e7b5f230013a0a19ca
panicbit <panicbit.dev@gmail.com>
2019-12-09 06:45:28 +0100

    day09_2

--- day09_2/src/main.rs
+++ day09_2/src/main.rs
@@ -1,6 +1,12 @@
 #[macro_use] extern crate aoc;
 
 #[aoc(2019, 09, 2)]
-fn main(input: &str) -> i32 {
-    unimplemented!()
+fn main(input: &str) -> isize {
+    let mem = intcode::parse(input);
+
+    let mut vm = intcode::VM::new(mem);
+    vm.add_input(2);
+    vm.run();
+
+    vm.outputs()[0]
 }
