cfbc66e8aa7ea87b11846c0acecb3c5a5a6a1864
panicbit <panicbit.dev@gmail.com>
2019-12-13 09:15:52 +0100

    day13

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -1,6 +1,23 @@
 #[macro_use] extern crate aoc;
 
+use std::collections::HashMap;
+
 #[aoc(2019, 13, 1)]
-fn main(input: &str) -> i32 {
-    unimplemented!()
+fn main(input: &str) -> usize {
+    let mem = intcode::parse(input);
+    let mut vm = intcode::VM::new(mem);
+
+    vm.run();
+
+    let mut field = HashMap::new();
+
+    for tile in vm.outputs().chunks(3) {
+        let x = tile[0];
+        let y = tile[1];
+        let id = tile[2];
+
+        field.insert((x, y), id);
+    }
+
+    field.values().filter(|&&id| id == 2).count()
 }
